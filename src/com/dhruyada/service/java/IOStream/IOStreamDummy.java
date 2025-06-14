2c0931719a98c1aeb9c508bd6fd6fe63bff3fa20
dhruyada1408 <dhruvyadav1408@gmail.com>
2025-06-14 18:47:18 +0530

    Add logic to create file without dir exist and then improve it still a bad code

--- src/com/dhruyada/service/java/IOStream/IOStreamDummy.java
+++ src/com/dhruyada/service/java/IOStream/IOStreamDummy.java
@@ -1,6 +1,10 @@
 package com.dhruyada.service.java.IOStream;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class IOStreamDummy {
 	
@@ -22,6 +26,73 @@ public class IOStreamDummy {
 		System.out.println("Path Separator: " + File.pathSeparator);
 		
 		System.out.println(file.getAbsolutePath());
+		
+		String exampleWriteString = "This is a test string " + System.lineSeparator() + "This is a new line";
+		
+		file = new File("csvDir/file.csv");
+		try {
+			file.createNewFile();	//Created a new empty file
+//			file.delete(); //this will delete the file or dir
+		} catch (IOException e) {
+			System.out.println("got error while creating file");
+			System.out.println("Checking if parent dir exists " + file.getParentFile().exists());
+			//lets ensure that folder exists 
+			file.getParentFile().mkdirs();
+			try {
+				file.createNewFile();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println("got error while creating file");
+			System.out.println("Checking if parent file exists now " + file.exists());
+		}
+		
+		
+//		Other Important apis
+		
+//		 Path Info
+//		 Method	Description
+//		 getName()	Returns the name of the file or directory
+//		 getPath()	Returns the path used to create the file
+//		 getAbsolutePath()	Returns the absolute path
+//		 getParent()	Returns the parent directory path
+//		 getCanonicalPath()	Returns the unique, absolute path after resolving . and ..
+		
+//		exists()	Checks if file or directory exists
+//		isFile()	Checks if it's a file
+//		isDirectory()	Checks if it's a directory
+//		canRead() / canWrite() / canExecute()	Checks permissions
+		
+//		list()	Lists names of files/directories as String[]
+//				listFiles()	Lists files/directories as File[]
+//				list(FilenameFilter)	Filtered list by name
+//				listFiles(FileFilter)	Filtered list by file properties
+		
+		
+//		Files.createFile(Path)
+//		Files.createDirectory(Path)
+//		Files.createDirectories(Path)
+//		Files.delete(Path)
+//		Files.exists(Path)
+//		Files.copy(source, target)
+//		Files.move(source, target)
+//		Files.write(Path, byte[] or List<String>)
+//		Files.readAllBytes(Path)
+//		Files.readAllLines(Path)
+		
+		
+		
+		File[] listFiles = file.listFiles(pathName -> 
+			pathName.getName().endsWith(".java"));
+		
+		
+		
+		
+		
+//		NIO class 
+		Path path = Paths.get("nioDir", "innerNioDir");
+		Files.isDirectory(path);
 		
 		
 	}
