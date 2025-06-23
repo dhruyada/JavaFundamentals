8a6e2138cad12d53777bd3c8cc6a0c47fccb8363
dhruyada1408 <dhruvyadav1408@gmail.com>
2025-06-24 00:01:22 +0530

    Implement deadlock using reentrant lock

--- src/com/dhruyada/service/java/multithreading/DeadLock.java
+++ src/com/dhruyada/service/java/multithreading/DeadLock.java
@@ -1,38 +1,30 @@
 package com.dhruyada.service.java.multithreading;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static java.lang.Thread.sleep;
 
 public class DeadLock {
+    static Lock lock1 = new ReentrantLock();
+    static Lock lock2 = new ReentrantLock();
     public static void main(String[] args) throws InterruptedException {
-        String lock1 = "Lock1";
-        String lock2 = "Lock2";
         Thread thread1 = new Thread(()->{
-            synchronized (lock1) {
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Thread1 here b**ches, lock1");
-                synchronized (lock2){
-                    System.out.println("Thread1 here b**ches, lock2");
-
-                }
-            }
+            lock1.lock();
+            System.out.println("Thread1 here b**ches, lock1");
+            lock2.lock();
+            System.out.println("Thread1 here b**ches, lock2");
+            lock1.unlock();
+            lock2.unlock();
         });
 
         Thread thread2 = new Thread(()->{
-            synchronized (lock2) {
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("Thread2 here b**ches, lock2");
-                synchronized (lock1){
-                    System.out.println("Thread2 here b**ches, lock1");
-                }
-            }
+            lock2.lock();
+            System.out.println("Thread2 here b**ches, lock2");
+            lock1.lock();
+            System.out.println("Thread2 here b**ches, lock1");
+            lock2.unlock();
+            lock1.unlock();
         });
 
         thread1.start();
