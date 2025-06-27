19db85d81b7d8770391149dc0e6e95ec0c723db0
dhruyada1408 <dhruvyadav1408@gmail.com>
2025-06-27 19:37:26 +0530

    Write using Buffered writer

--- src/com/dhruyada/service/java/IOStream/IOStreamHaha.java
+++ src/com/dhruyada/service/java/IOStream/IOStreamHaha.java
@@ -1,6 +1,8 @@
 package com.dhruyada.service.java.IOStream;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class IOStreamHaha {
@@ -11,11 +13,20 @@ public class IOStreamHaha {
         file.createNewFile();
 
         File file_folder = new File("src/example_folder");
+        // mkdirs can create complete path
         if(file_folder.mkdir()){
             System.out.println("Folder Created");
         }else {
             System.out.println("Folder already exists");
         }
-    }
 
+        /**
+         * Writing Content in a file
+         */
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/example_java_file.txt"))){
+            bufferedWriter.write("some string in file");
+        } catch (IOException e){
+            System.out.println("IOException");
+        }
+    }
 }
