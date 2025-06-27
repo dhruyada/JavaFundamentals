78e342fd5cc8cfd5f946ad2439221db92523266f
dhruyada1408 <dhruvyadav1408@gmail.com>
2025-06-27 19:31:51 +0530

    Create folder using java io file

--- src/com/dhruyada/service/java/IOStream/IOStreamHaha.java
+++ src/com/dhruyada/service/java/IOStream/IOStreamHaha.java
@@ -9,6 +9,13 @@ public class IOStreamHaha {
     public static void main(String[] args) throws IOException {
         File file = new File("src/example_java_file.txt");
         file.createNewFile();
+
+        File file_folder = new File("src/example_folder");
+        if(file_folder.mkdir()){
+            System.out.println("Folder Created");
+        }else {
+            System.out.println("Folder already exists");
+        }
     }
 
 }
